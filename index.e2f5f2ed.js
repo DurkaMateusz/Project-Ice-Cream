09236c0efb367feb3955eb95fc0c14c57d1d7b1b
DurkaMateusz <121753983+DurkaMateusz@users.noreply.github.com>
2026-01-22 13:13:14 +0000

    Deploying to gh-pages from @ DurkaMateusz/Project-Ice-Cream@42f685ab03f7739d1955627021b6e1c2b48bfdb1 🚀

--- index.e2f5f2ed.js
+++
@@ -1,3 +0,0 @@
-var e=0;!function l(){var t,n=document.getElementsByClassName("mySlides ");for(t=0;t<n.length;t++)n[t].style.display="none ";++e>n.length&&(e=1),n[e-1].style.display="block ",setTimeout(l,1e4)}();var l=1;!function(e){var t,n=document.getElementsByClassName("mySlides"),s=document.getElementsByClassName("demo");for(e>n.length&&(l=1),e<1&&(l=n.length),t=0;t<n.length;t++)n[t].style.display="none";for(t=0;t<s.length;t++);n[l-1].style.display="block"}//# sourceMappingURL=index.e2f5f2ed.js.map
-(1);
-//# sourceMappingURL=index.e2f5f2ed.js.map
